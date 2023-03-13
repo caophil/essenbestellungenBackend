56900a9035b9bd7bfb9f64678ae57ee830d1eba5
caophil <philippcao60@gmail.com>
2023-03-13 15:51:38 +0100

    small changes

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -12,7 +12,6 @@ export class LocalStrategy extends PassportStrategy(Strategy){
     async validate(userName: string, password: string): Promise<any>{
         const username = userName.toLowerCase();
         const user = await this.authService.validateUser(username,password);
-        console.log("hier");
         
         if(!user){
             throw new UnauthorizedException();
