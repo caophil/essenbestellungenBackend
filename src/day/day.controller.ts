9f3a709da52fa115baf66557074c424afb07b946
caophil <philippcao60@gmail.com>
2023-03-24 14:09:42 +0100

    CRUD for Days

--- src/day/day.controller.ts
+++ src/day/day.controller.ts
@@ -1,4 +1,5 @@
 import {AdminGuard} from 'src/auth/admin.guard';
+import {AuthenticatedGuard} from "src/auth/authenticated.guard";
 import { Controller, Post, Body, Get, Param, Put, Delete, UseGuards} from '@nestjs/common';
 import{DayService} from "./day.service";
 import {DayDocument, Day} from "./day";
@@ -13,5 +14,29 @@ export class DayController {
     @Post()
     createDay(@Body() day: Day): Promise<DayDocument>{
         return this.dayService.createDay(day);
+    }
+
+    @UseGuards(AuthenticatedGuard)
+    @Get()
+    getDays():Promise<DayDocument[]>{
+        return this.dayService.getDays();
+    }
+
+    @UseGuards(AuthenticatedGuard)
+    @Get(':id')
+    getDay(@Param('id')id: string):Promise<DayDocument>{
+        return this.dayService.getDay(id);
+    }
+
+    @UseGuards(AdminGuard)
+    @Put(':id')
+    updateDay(@Param('id')id: string, @Body()newDay: Day):Promise<DayDocument>{
+        return this.dayService.updateDay(id,newDay);
+    }
+
+    @UseGuards(AdminGuard)
+    @Delete(':id')
+    deleteDay(@Param('id')id: string){
+        return this.dayService.deleteDay(id);
     }
 }
--- src/day/day.service.ts
+++ src/day/day.service.ts
@@ -13,4 +13,26 @@ export class DayService {
         return dayDoc.save();
     }
 
+    getDays():Promise<DayDocument[]>{
+        return this.dayModel.find().exec();
+    }
+    getDay(id: string):Promise<DayDocument>{
+        return this.dayModel.findById(id).exec();
+    }
+
+    async updateDay(id: string, newDay: Day): Promise<DayDocument>{
+        const oldDay = await this.getDay(id);
+        oldDay.date = newDay.date ? newDay.date : newDay.date;
+        oldDay.menu1 = newDay.menu1 ? newDay.menu1 : newDay.menu1;
+        oldDay.menu2 = newDay.menu2 ? newDay.menu2 : newDay.menu2;
+        oldDay.nachtisch = newDay.nachtisch ? newDay.nachtisch : newDay.nachtisch;
+        oldDay.suppe = newDay.suppe ? newDay.suppe : newDay.suppe;
+        return oldDay.save();
+    }
+
+    deleteDay(id: string){
+        return this.dayModel.deleteOne({
+            _id: id
+        })
+    }
 }
