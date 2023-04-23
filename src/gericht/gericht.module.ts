fd7c1243066e6c0c2083b69cffd1aa445ac94642
HannesWeizmann <hannesweizmann54@gmail.com>
2023-04-23 11:33:42 +0200

    fix, damit es wieder läuft

--- src/gericht/gericht.module.ts
+++ src/gericht/gericht.module.ts
@@ -2,26 +2,14 @@ import { Module } from '@nestjs/common';
 import { GerichtService } from './gericht.service';
 import { GerichtController } from './gericht.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { GerichtSchema, Gericht, GerichtDocument } from './gericht';
-import { Model } from 'mongoose';
+import {GerichtSchema, Gericht} from "./gericht";
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([
-      {
-        name: Gericht.name,
-        schema: GerichtSchema,
-      },
-    ]),
-  ],
-  providers: [
-    GerichtService,
-    {
-      provide: 'GerichtModel',
-      useFactory: (gerichtModel: Model<GerichtDocument>) => gerichtModel,
-      inject: [Gericht.name],
-    },
-  ],
-  controllers: [GerichtController],
+  imports:[MongooseModule.forFeature([{
+    schema: GerichtSchema,
+    name: Gericht.name
+  }])],
+  providers: [GerichtService],
+  controllers: [GerichtController]
 })
 export class GerichtModule {}
