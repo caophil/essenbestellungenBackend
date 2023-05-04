bceefba4f5e8e0dac5c874bd2925dafa40b81a35
HannesWeizmann <hannesweizmann54@gmail.com>
2023-05-04 09:36:19 +0200

    auskommentierter test entfernt

--- e2e/cypress/e2e/user.cy.js
+++ e2e/cypress/e2e/user.cy.js
@@ -38,47 +38,4 @@ describe('User', () => {
 
     cy.get('button[type="login"]').click();
   });
-
-/*
-  it('should log out a logged in user', () => {
-    const user = {
-      username: 'testuser',
-      password: 'testpassword',
-      email: 'testuser@example.com',
-      firstname: 'Test',
-      lastname: 'User',
-      personalnummer: 12345,
-      role: 'user',
-    };
-
-    cy.visit('localhost:4200/sign-up');
-
-    cy.contains('Sign up').click();
-
-    cy.get('input[name="username"]').type(user.username);
-    cy.get('input[name="email"]').type(user.email);
-    cy.get('input[name="firstname"]').type(user.firstname);
-    cy.get('input[name="lastname"]').type(user.lastname);
-    cy.get('input[name="personalnummer"]').type(user.personalnummer);
-    cy.get('input[name="password"]').type(user.password);
-
-    cy.get('button[type="Sign-up"]').click();
-
-    cy.visit('localhost:4200/login');
-
-    cy.contains('Log in').click();
-
-    cy.get('input[name="username"]').type(user.username);
-    cy.get('input[name="password"]').type(user.password);
-
-    cy.get('button[type="login"]').click();
-
-    //cy.url().should('include', '/protected');
-
-    cy.contains('Log out').click();
-
-    cy.get('button[type="logout"]').click();
-
-    //cy.url().should('include', '/login');
-  });*/
 });
